739e3024a5cf555446af7c67c11193d08bdcaa05
YanEmil <yanbaev.emil.work@gmail.com>
2021-03-09 23:24:11 +0300

    Updates

--- static/js/5.3d79e651.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[5],{283:function(p,s,t){"use strict";t.r(s);var c=t(7);s.default=function(){return Object(c.jsx)("div",{children:"search"})}}}]);
-//# sourceMappingURL=5.3d79e651.chunk.js.map
